03a779af2dc9f8f9c95813f8eb957dd5b8b86e5f
PeterChou <peter820307@gmail.com>
2016-09-01 15:42:38 +0800

    Fb login test (without storing in any Database) 1.Integrate FBSDK 2.Create login button 3.Get user's profile , email, and pic(url)
    but still trying to figure out the process...

---
+++ MADLoginDemo/NSMutableArray+test.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+test.h
+//  MADLoginDemo
+//
+//  Created by 周大剛 on 2016/9/1.
+//  Copyright © 2016年 周大剛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (test)
+
+@end
---
+++ MADLoginDemo/NSMutableArray+test.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+test.m
+//  MADLoginDemo
+//
+//  Created by 周大剛 on 2016/9/1.
+//  Copyright © 2016年 周大剛. All rights reserved.
+//
+
+#import "NSMutableArray+test.h"
+
+@implementation NSMutableArray (test)
+
+@end
