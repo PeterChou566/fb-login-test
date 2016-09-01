1f6eec6719f780b6e66f856b122780c11959604f
PeterChou <peter820307@gmail.com>
2016-09-01 13:19:58 +0800

    Initial Commit

---
+++ MADLoginDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MADLoginDemo
+//
+//  Created by 周大剛 on 2016/9/1.
+//  Copyright © 2016年 周大剛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
